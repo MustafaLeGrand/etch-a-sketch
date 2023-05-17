4fe9778b059234c49b21c6d07e2ab7ee562828ec
Raul Garcia Lagunas <rgcoloman@gmail.com>
2023-05-17 16:36:40 -0600

    add comments to js code, add dinamic size of pixels in grid

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,25 @@
-let n = 100;
+let n = 64;
 let totalSquares = n * n;
+
+let size = 650/n;
 
 let makeGrid = () =>
 {
+    let x = `${size}px`;
+
+    // Access the container element
     let container = document.getElementById('container');
+    // Manipulate the the style to set the rows and cols with css variables
     container.style.setProperty('--rows', n);
     container.style.setProperty('--cols', n);
+    // Edit the grid pixel size
+    container.style.setProperty('--size', x);
+
+    console.log(size);
+
+    console.log(typeof x);
+
+
 
     for(let i = 0; i < (totalSquares); i++)
     {
@@ -16,4 +30,19 @@ let makeGrid = () =>
 
 }
 
+let getSquareSize = () =>
+{
+    let size = document.getElementById('size');
+
+    size.addEventListener('click', size)
+    {
+        console.log(size.min);
+    }
+
+
+}
+
+
+
+getSquareSize();
 makeGrid();
