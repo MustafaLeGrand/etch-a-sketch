9be105dd277fc3cb12d4e0c52ea96e055c0522fa
Raul Garcia Lagunas <rgcoloman@gmail.com>
2023-05-18 11:43:59 -0600

    add getGridSize function to use input range button and size displays dinamically

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,34 @@
-let n = 100;
+let value = document.getElementById('value');
+let input = document.getElementById('input');
+
+let getGridSize = () =>
+{
+    value.textContent = input.value;
+
+    input.addEventListener('input', () => {
+        value.textContent = input.value;
+    });
+}
 
 let makeGrid = () =>
 {
+    // Input
+    //let n = ;
+
     // Grid size variables
     let totalPixels = n * n;
-    let sizeOfPixel = 650/n;
+    const gridSide = 650;
+    let sizeOfPixel = gridSide/n;
 
     // Size of each pixel in the grid converted into string plus the px
     let x = `${sizeOfPixel}px`;
 
     // Access the container element
     let container = document.getElementById('container');
-    // Manipulate the the style to set the rows and cols with css variables
+    // Manipulate the the style to set the rows, cols
+    // & grid pixel size with css variables
     container.style.setProperty('--rows', n);
     container.style.setProperty('--cols', n);
-    // Edit the grid pixel size
     container.style.setProperty('--size', x);
 
     // Cycle to add every pixel in the grid
@@ -27,22 +41,7 @@ let makeGrid = () =>
         // Appending the div to form the grid
         container.appendChild(square);
     }
-
 }
 
-let getSquareSize = () =>
-{
-    let size = document.getElementById('size');
-
-    size.addEventListener('click', size)
-    {
-        console.log(size.min);
-    }
-
-
-}
-
-
-
-getSquareSize();
+getGridSize();
 makeGrid();
